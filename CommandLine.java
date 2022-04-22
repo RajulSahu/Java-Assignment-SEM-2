9c252a97317ebcde35f9994f11098112c5f0bba6
Rajul Sahu <74370799+RajulSahu@users.noreply.github.com>
2022-04-22 21:44:49 +0530

    Update CommandLine.java

--- CommandLine.java
+++ CommandLine.java
@@ -1,5 +1,19 @@
 package practical_programs;
 
-public class CommandLine {
+public class Constructor {
+    public static void main(String[] args) {
+        Second obj = new Second();
+        int res = obj.findSum(args);
+        System.out.println(res);
+    }
 
+}
+class Second {
+    public int findSum(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < args.length; i++) {
+            sum += Integer.parseInt(args[i]);
+        }
+        return sum;
+    }
 }
