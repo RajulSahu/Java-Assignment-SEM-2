5d60492a28b22c01383490a7ccff484d4f0691a4
Rajul Sahu <74370799+RajulSahu@users.noreply.github.com>
2022-04-22 19:48:34 +0900

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,13 @@
+package practical_programs;
+
+public class Constructor {
+
+	public static void main(String args[]) {
+		Fifth obj = new Fifth();
+	}
+}
+class Fifth {
+	Fifth(){
+		System.out.println("Object is Created, default constructor called..");
+	}
+}
